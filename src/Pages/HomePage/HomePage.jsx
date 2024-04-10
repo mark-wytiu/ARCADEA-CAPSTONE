ef7a730ace589d4a7a7eaaae9570f682f528eee5
mark-wytiu <mark.wytiu@hotmail.com>
2024-04-10 11:10:46 -0700

    add gamepagedetails component

--- src/Pages/HomePage/HomePage.jsx
+++ src/Pages/HomePage/HomePage.jsx
@@ -4,11 +4,14 @@ import Grid from '@mui/material/Unstable_Grid2';
 import Box from '@mui/material/Box';
 import bgImg from "../../Assets/Images/carl-raw-m3hn2Kn5Bns-unsplash.jpg"
 
+
 function HomePage() {
 
+
+
+
     return (
-
-
+        
         <Box sx={{ width: '100%', backgroundImage:`url(${bgImg})`, backgroundRepeat: "no-repeat",
         backgroundSize: "cover", height:'80rem' }}>
             <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3  }}>
