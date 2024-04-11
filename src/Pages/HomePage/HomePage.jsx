ccbb1548703ef18f6b404a217adbe21138c90fa4
mark-wytiu <mark.wytiu@hotmail.com>
2024-04-11 15:16:54 -0700

    add axios in homepage

--- src/Pages/HomePage/HomePage.jsx
+++ src/Pages/HomePage/HomePage.jsx
@@ -2,11 +2,43 @@ import GameCard from "../../Components/GameCard/GameCard";
 import Grid from '@mui/material/Unstable_Grid2';
 import Box from '@mui/material/Box';
 import bgImg from "../../Assets/Images/carl-raw-m3hn2Kn5Bns-unsplash.jpg"
+import { Axios } from "axios";
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
 
 
 function HomePage() {
+    const [games, setGames] = useState([]);
+	const [selectedGame, setSelectedGame] = useState({});
+	const baseUrl2 = process.env.REACT_APP_BASE_REACT_URL;
+
+	const { gameId } = useParams();
 
 
+    useEffect(() => {
+		const getGames = async () => {
+			const response = await axios.get(`${baseUrl2}/games`);
+			const allGames = response.data;
+			setGames(allGames);
+		};
+		getGames();
+	}, []);
+
+	useEffect(() => {
+
+
+		const gameData = async (id) => {
+			const response = await axios.get(`${baseUrl2}/videos/${id}`);
+			setSelectedVideo(response.data);
+			console.log(response.data);
+		};
+
+		if (videoId) {
+			videoData(videoId);
+		} else {
+			videoData(defaultId);
+		}
+	}, [videoId]);
 
 
     return (
